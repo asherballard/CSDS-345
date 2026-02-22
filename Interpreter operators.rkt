aa6f72204d899351830688d2f6b1fda2068b3ac9
asherballard <108826020+asherballard@users.noreply.github.com>
2026-02-21 18:20:42 -0800

    Add files via upload

--- Interpreter operators.rkt
+++ Interpreter operators.rkt
@@ -2,26 +2,6 @@
 
 
 (define addition
-  (lambda (x y)
-    (+ x y)))
-
-(define subtraction
-  (lambda (x y)
-    (- x y)))
-
-(define multiplication
-  (lambda (x y)
-    (* x y)))
-
-(define division
-  (lambda (x y)
-    (/ x y)))
-
-(define unary
-  (lambda (x)
-    (* x -1)))
-
-(define modulo
-  (lambda (x y)
-    (modulo x y)))
-  
+  (lambda (lis)
+    (cond
+      (and (number? 
