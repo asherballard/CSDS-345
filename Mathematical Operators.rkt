98381c933402a607a84d2030de7b37bea0a0f1c5
asherballard <108826020+asherballard@users.noreply.github.com>
2026-02-21 19:29:54 -0800

    Add files via upload

---
+++ Mathematical Operators.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+
+(define addition
+  (lambda (lis return)
+    (cond
+      ((null? lis) 0)
+      ((null? (cdr lis)) (car lis))
+      (addition (cdr lis) (lambda (sum) (return (+ sum (+ (car lis) (cdr lis)))))))))
+
+(define subtraction
+  (lambda (lis x return)
+    (cond
+      ((null? lis) 0)
+      ((and (null? (cdr lis)) (eq? x 1)) (* -1 (car lis)))
+      ((null? (cdr lis)) (car lis))
+      (subtraction (cdr lis) 0 (lambda (difference) (return (- difference (car lis))))))))
+
+(define multiplication
+  (lambda (lis return)
+    (cond
+      ((null? lis) 0)
+      ((null? (cdr lis)) (car lis))
+      (multiplication (cdr lis) (lambda (product) (return (* product (* (car lis) (cdr lis)))))))))
+
+;needs to be fixed
+(define division
+  (lambda (lis return)
+    (cond
+      ((null? lis) 0)
+      ((null? (cdr lis)) (car lis))
+      (division (cdr lis) (lambda (quotient) (return (/ (/ (car lis) (cdr lis)))))))))
+
+(define modulo*
+  (lambda (lis return)
+    (cond
+      ((null? lis) 0)
+      ((null? (cdr lis)) (car lis))
+      (modulo* (cdr lis) (lambda (remainder) (return (modulo remainder (modulo (car lis) (cdr lis)))))))))
+
+(define unary
+  (lambda (x)
+    (* x -1)))
+      
+      
