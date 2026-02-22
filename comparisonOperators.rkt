030b6f710ab7707f90c2db5c5ad12df7e774449f
asherballard <108826020+asherballard@users.noreply.github.com>
2026-02-21 20:09:20 -0800

    Update comparisonOperators.rkt

--- comparisonOperators.rkt
+++ comparisonOperators.rkt
@@ -1,28 +1,45 @@
 #lang racket
-(provide (all-defined-out))
 
 (define equal
   (lambda (lis return)
     (cond
-      ((null? lis) #t)
-      (equal (cdr lis) (lambda (equals) (return 
+      ((null? (cdr lis)) #t)
+      ((not (eq? (car lis) (cdr lis))) (return #f))
+      (equal (cdr lis) echo))))
 
-(define notEqual
-  (lambda (x y)
-    (not (equal? x y))))
+(define notequal
+  (lambda (lis return)
+    (cond
+      ((null? (cdr lis)) #t)
+      ((eq? (car lis) (cdr lis))) (return #f)
+      (notequal (cdr lis) echo)))
 
-(define greater
-  (lambda (x y)
-    (> x y)))
+  (define greater
+    (lambda (lis return)
+      (cond
+        ((null? (cdr lis)) #t)
+        ((not (> (car lis) (cdr lis))) (return #f))
+        (greater (cdr lis) echo))))
 
-(define lesser
-  (lambda (x y)
-    (< x y)))
 
-(define greaterOrEqual
-  (lambda (x y)
-    (not (lesser x y))))
+  (define lesser
+    (lambda (lis return)
+      (cond
+        ((null? (cdr lis)) #t)
+        ((not (< (car lis) (cdr lis))) (return #f))
+        (lesser (cdr lis) echo))))
 
-(define lesserOrEqual
-  (lambda (x y)
-    (not (greater x y))))
+  (define greaterorequal
+    (lambda (lis return)
+      (cond
+        ((null? (cdr lis)) #t)
+        ((< (car lis) (cdr lis)) (return #f))
+        (greaterorequal (cdr lis) echo))))
+
+
+  (define lessorequal
+    (lambda (lis return)
+      (cond
+        ((null? (cdr lis)) #t)
+        ((> (car lis) (cdr lis)) (return #f))
+        (lessorequal (cdr lis) echo))))
