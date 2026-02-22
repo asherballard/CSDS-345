780f5695b8b6bcdf552f2256f68663a61676877a
asherballard <108826020+asherballard@users.noreply.github.com>
2026-02-21 19:17:20 -0800

    Add files via upload

---
+++ Conditional Operators.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+;conditionals
+
+(define condition
+  (lambda (lis state)
+    (lineParser (car lis) state)))
+
+(define body
+  (lambda (lis state)
+    (lineParser (cadr lis) state)))
+
+
+(define else*
+  (lambda (lis state)
+    (cond
+      (eq? 'if (car lis) (cdr (cdr (cdr lis))))
+      (cdr (cdr lis)))))
+
+(define if*
+  (lambda (lis state)
+    (cond
+      ((eq? (car lis) 'return) (body lis state))
+      ((condition lis state) (body lis state))
+      (if* (else* lis state) state))))
+      
+
+(define while
+  (lambda (lis state return)
+    (cond
+      ((condition lis state) (while lis state (lambda (loop) (lineParser (body lis) echo))))
+      state)))
+
+
+
